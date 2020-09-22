51d1ea3c352d01ad73b6339d2d66cc4530fe4bdb
Luigi Foscari <luigi.foscari@icloud.com>
2020-09-22 20:40:26 +0200

    python notations are dumb

--- bot.py
+++ bot.py
@@ -7,14 +7,12 @@ try: from instanceElements import USERS
 except: USERS = None
 
 def log(message, username):
-    """Log to stdin"""
     now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     print("[{}] {} from @{}".format(now, message, username))
 
 # Authentication
 
-def auth(func):
-    """Authenticate user from USERS"""
+def authentication(func):
     def inner(*args, **kwargs):
         if USERS is None or args[0].message.from_user.id in USERS:
             return func(*args, **kwargs)
@@ -27,30 +25,26 @@ def error(update, context):
 
 # Torrent manipulation
 
-def load_torrent(path):
-    """Add torrent to queue"""
+def add_torrent(path):
     if not 0:
     # if not os.system("transmission-remote -a {}".format(url))
         return "I got you bro, just wait for it"
     return "oh dear, I'm terribly sorry, but something went wrong"
 
-@auth
-def load_torrent_from_path(update, context):
-    """Extract uri or magnet from user's message"""
+@authentication
+def text_to_path(update, context):
     torrent_path = update.message.text
     log("new torrent", update.message.from_user.username)
-    update.message.reply_text(load_torrent(torrent_path))
+    update.message.reply_text(add_torrent(torrent_path))
 
-@auth
-def load_torrent_from_file(update, context):
-    """Extract torrent file path from user's message"""
+@authentication
+def file_to_path(update, context):
     torrent_path = update.message.document.get_file().file_path
     log("new torrent", update.message.from_user.username)
-    update.message.reply_text(load_torrent(torrent_path))
+    update.message.reply_text(add_torrent(torrent_path))
 
-@auth
+@authentication
 def list_torrents(update, context):
-    """Get all torrents"""
     log("/list", update.message.from_user.username)
     # os.system("transmission-remote -l")
     update.message.reply_text("I gotta work on that")
@@ -60,8 +54,8 @@ if __name__ == '__main__':
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("list", list_torrents))
-    dp.add_handler(MessageHandler(Filters.text, load_torrent_from_path))
-    dp.add_handler(MessageHandler(Filters.document, load_torrent_from_file))
+    dp.add_handler(MessageHandler(Filters.text, text_to_path))
+    dp.add_handler(MessageHandler(Filters.document, file_to_path))
 
     print("Now listening...")
 
